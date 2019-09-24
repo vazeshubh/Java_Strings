91b795ba279558929d4aaafe16d9ca3189116ee2
vazeshubh <55427312+vazeshubh@users.noreply.github.com>
2019-09-24 17:22:44 +0530

    Add files via upload

---
+++ p4.java
@@ -0,0 +1,19 @@
+package com.durgasoft.strings;
+
+public class p4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s=new String("durga");
+		System.out.println(s);//durga
+		
+		s.concat("software");
+		System.out.println(s);//durga
+		
+		s=s.concat("solutions");
+		System.out.println(s);//durgasolutions
+
+	}
+
+}
